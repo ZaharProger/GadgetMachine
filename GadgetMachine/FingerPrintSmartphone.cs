785376b2418115dd117d04956bf7066ec76d680b
Захар Домолего <DomolegoZahar@gmail.com>
2021-10-22 13:08:35 +0800

    Добавил декораторы смартфонов с отпечатком пальца и NFC

---
+++ GadgetMachine/FingerPrintSmartphone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GadgetMachine
+{
+    //Декоратор смартфона в смартфон с отпечатком пальца
+    class FingerPrintSmartphone : Decorator<Smartphone>
+    {
+        public FingerPrintSmartphone(Smartphone smartphone) : base (smartphone)
+        {}
+
+        public override string GetInfo()
+        {
+            return gadget.GetInfo() + "\nНаличие сканнера отпечатка пальца: Да";
+        }
+    }
+}
---
+++ GadgetMachine/NFCSmartphone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GadgetMachine
+{
+    class NFCSmartphone
+    {
+        //Декоратор смартфона в смартфон с NFC
+        class FingerPrintSmartphone : Decorator<Smartphone>
+        {
+            public FingerPrintSmartphone(Smartphone smartphone) : base(smartphone)
+            { }
+
+            public override string GetInfo()
+            {
+                return gadget.GetInfo() + "\nНаличие бесконтактной оплаты NFC: Да";
+            }
+        }
+    }
+}
