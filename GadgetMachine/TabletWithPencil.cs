78117ff849c0e9415c4351f91ae44fc4c4c9841f
Захар Домолего <DomolegoZahar@gmail.com>
2021-10-22 13:11:33 +0800

    Добавил декоратор Планшет со стилусом

---
+++ GadgetMachine/TabletWithPencil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GadgetMachine
+{
+    //Декоратор планшета со стилусом
+    class TabletWithPencil : Decorator<Tablet>
+    {
+        public TabletWithPencil(Tablet tablet) : base(tablet)
+        { }
+
+        public override string GetInfo()
+        {
+            return gadget.GetInfo() + "\nНаличие стилуса: Да";
+        }
+    }
+}
