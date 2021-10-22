0dc45a9295b70e6dc0eea62cb1d6f14fb657fa91
Захар Домолего <DomolegoZahar@gmail.com>
2021-10-22 11:04:12 +0800

    Добавил абстрактный класс Гаджетов

---
+++ GadgetMachine/Gadget.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GadgetMachine
+{
+    //Класс гаджетов
+    abstract class Gadget
+    {
+        //Структура дисплея: ширина и высота
+        struct Display
+        {
+            public short width;
+            public short height;
+        }
+        Display display;
+
+        protected Gadget(short width, short height)
+        {
+            display.width = width;
+            display.height = height;
+        }
+
+        public abstract string GetInfo();
+    }
+}
